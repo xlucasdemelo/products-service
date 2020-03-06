363799cab41b3ed5a60264579cb1708fe0da51a4
Lucas <lucas.meloaguiar@gmail.com>
2020-03-05 23:20:33 -0300

    Adding cors filter to allow all requests

--- src/main/java/com/asellion/productsservice/ProductsServiceApplication.java
+++ src/main/java/com/asellion/productsservice/ProductsServiceApplication.java
@@ -1,9 +1,16 @@
 package com.asellion.productsservice;
 
+import java.util.Collections;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ProductsServiceApplication {
@@ -11,6 +18,20 @@ public class ProductsServiceApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 	    return new ModelMapper();
+	}
+	
+	@Bean
+	public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowCredentials(true);
+	    config.setAllowedOrigins(Collections.singletonList("*"));
+	    config.setAllowedMethods(Collections.singletonList("*"));
+	    config.setAllowedHeaders(Collections.singletonList("*"));
+	    source.registerCorsConfiguration("/**", config);
+	    FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+	    bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+	    return bean;
 	}
 	
 	public static void main(String[] args) {
--- src/main/java/com/asellion/productsservice/controller/JwtAuthenticationController.java
+++ src/main/java/com/asellion/productsservice/controller/JwtAuthenticationController.java
@@ -19,7 +19,6 @@ import com.asellion.productsservice.security.JwtTokenUtil;
 import com.asellion.productsservice.service.JwtUserDetailsService;
 
 @RestController
-@CrossOrigin
 public class JwtAuthenticationController {
 	
 	@Autowired
